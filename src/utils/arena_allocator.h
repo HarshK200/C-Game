0ba9ed38a0817fc76eb940fd189294fb2b271c3c
HarshK200 <harshkapadia04@gmail.com>
2026-09-08 01:40:57 +0530

    added arena reset and ArenaAlloc now memsets the memory to 0

--- src/utils/arena_allocator.h
+++ src/utils/arena_allocator.h
@@ -49,6 +49,7 @@ inline ArenaAllocator CreateArena(size_t size)
     to return a 16 bytes aligned pointer *Which is does on 64 bit systems*
 
     Allocates the memory of *size_t size* using the arena passed in.
+    The returned memory *IS SET TO ZERO* using memset()
     Returns a char* to allocated memory on success, otherwise returns nullptr on failure.
 */
 inline char* ArenaAlloc(ArenaAllocator* arena_allocator, size_t size)
@@ -70,7 +71,20 @@ inline char* ArenaAlloc(ArenaAllocator* arena_allocator, size_t size)
     }
 
     result = arena_allocator->base + arena_allocator->used;
+    // zero out the memory
+    memset((void*)result, 0, alignment_size);
     arena_allocator->used += alignment_size;
 
     return result;
+}
+
+/*
+    NOTE(harsh): this *DOES NOT* zero out the memory of the arena it just resets the
+    arena allocator used pointer to 0.
+
+    Resets the arena used pointer to 0, hence reseting the arena
+*/
+inline void ArenaReset(ArenaAllocator* arena_allocator)
+{
+    arena_allocator->used = 0;
 }
